b9e8e49eece4b3a37573cddf7765fd2a1d871145
Grant Gollier <gragollier@gmail.com>
2019-10-27 10:59:57 -0500

    Add post creation

--- social-stories-backend/routes/stories.js
+++ social-stories-backend/routes/stories.js
@@ -1,7 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const client = require('../utils/elastic-client');
+const uuid = require('uuid');
 
+// Get list of all courses
 router.get('/', async (req, res) => {
     const result = await client.search({
         index: 'course',
@@ -10,6 +12,7 @@ router.get('/', async (req, res) => {
     res.json(result.body.hits.hits.map(x => x['_source']));
 });
 
+// Search for course by title
 router.get('/search', async (req, res) => {
     const result = await client.search({
         index: 'course',
@@ -28,6 +31,7 @@ router.get('/search', async (req, res) => {
     res.json(result.body.hits.hits.map(x => x['_source']));
 });
 
+// DEPRECATED?
 router.get('/:course_id', async (req, res) => {
     const result = await client.search({
         index: req.params['course_id'].toLowerCase(),
@@ -36,6 +40,7 @@ router.get('/:course_id', async (req, res) => {
     res.json(result.body.hits.hits.map(x => x['_source']));
 });
 
+// Search for posts under a course
 router.get('/:course_id/search', async (req, res) => {
     const valid_courses = await client.search({
         index: 'course',
@@ -72,7 +77,45 @@ router.get('/:course_id/search', async (req, res) => {
     }
 });
 
+// Get all posts corresponding to a course
+router.get('/:course_id/post', async (req, res) => {
+    const response = await client.search({
+        index: req.params['course_id'].toLowerCase(),
+        body: {}
+    });
+    res.json(response.body.hits.hits.map(x => x['_source']));
+});
 
+// Create new post under a course
+router.post('/:course_id/post', async (req, res) => {
+    const id = uuid.v4();
+    const response = await client.create({
+        index: req.params['course_id'].toLowerCase(),
+        type: '_doc',
+        id: id,
+        body: {
+            title: req.body.title,
+            content: req.body.content,
+            id: id
+        }
+    });
+    res.sendStatus(response.statusCode);
+})
+
+// Get course by id
+router.get('/:course_id/post/:post_id', async (req, res) => {
+    try {
+        const response = await client.get({
+            index: req.params['course_id'].toLowerCase(),
+            type: '_doc',
+            id: req.params['post_id']
+        });
+        res.json(response.body['_source']);
+    } catch (e) {
+        console.error(e);
+        res.sendStatus(500);
+    }
+})
 
 
 module.exports = router;
--- social-stories-backend/server.js
+++ social-stories-backend/server.js
@@ -1,13 +1,17 @@
 const express = require("express");
 const morgan = require("morgan");
-const storiesRoute = require("./routes/stories");
-
 const bodyParser = require('body-parser');
+
+const storiesRoute = require("./routes/stories");
 
 const app = express();
 const port = 9000;
+
 app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(morgan('dev'));
 
 app.use("/course", storiesRoute);
-app.listen(port);
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});

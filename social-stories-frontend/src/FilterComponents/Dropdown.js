6de8ce5f2d9f25460182ee173a09fdd035c8a7c3
Antonette <amgichohu@gmail.com>
2019-10-31 20:22:42 -0500

    moving over to ubuntu

--- social-stories-frontend/src/FilterComponents/Dropdown.js
+++ social-stories-frontend/src/FilterComponents/Dropdown.js
@@ -1,5 +1,29 @@
 import React from 'react'
 
+
+let courses= [ 
+    {
+        name: "EECS 448",
+        content: "This is a cool class. Learn Github",
+        creator: "Antonette Gichohu"
+    },
+    {
+        name: "EECS 210",
+        content: "One of the harder courses, definitely memorize what they tell you to",
+        creator: "Karen Setiawan"
+    },
+    {
+        name: "EECS 368",
+        content: "Great stuff",
+        creator: "Antonette Gichohu"
+    },
+    {
+        name: "EECS 268",
+        content: "Lol, good luck",
+        creator: "Antonette Gichohu"
+    }
+];
+
 class Dropdown extends React.Component()
 {
     state = {
@@ -9,7 +33,7 @@ class Dropdown extends React.Component()
     }
     componentDidMount()
     {
-        Window.fetch('/api/course')
+        window.fetch('/api/course')
         .then((response) => {
             return response.json();
           })

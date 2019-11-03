e837420b398df1d55486ee0767210c598d241c8d
Antonette <amgichohu@gmail.com>
2019-11-02 21:04:49 -0500

    Documentation for dropdown

--- social-stories-frontend/src/App.js
+++ social-stories-frontend/src/App.js
@@ -5,6 +5,15 @@ import './App.css';
 import Dropdown from './FilterComponents/Dropdown';
 
 class App extends React.Component{
+
+  /**
+ * Constructor for the app
+ * @name constructor
+ * @memberof module:component
+ * @function 
+ * @param props: variables needed by this method
+ * @returns none
+ */
   constructor(props) {
     super(props);
     this.state = {
@@ -12,6 +21,8 @@ class App extends React.Component{
       searchParam: "6",
       data: []
     }
+
+
     this.setCourse = async (courseName) => {
       await this.setState({courseName: courseName});
       await this.updateView();
--- social-stories-frontend/src/FilterComponents/Dropdown.js
+++ social-stories-frontend/src/FilterComponents/Dropdown.js
@@ -1,7 +1,20 @@
 import React from 'react'
 import Select from 'react-select'
 
+/**
+ * Dropdown module
+ * @module FilterComponent/Dropdown
+ * @requires none
+ */
+
 class Dropdown extends React.Component {
+    /**
+     * Constructor
+     * @name constructor
+     * @memberof module: FilterComponent/Dropdown
+     * @function constructor
+     * @returns none
+    */
     constructor() {
         super();
         this.state = {
@@ -9,19 +22,39 @@ class Dropdown extends React.Component {
             selectedCourse: {},
             validationError: "",
         }
+        /**
+         * onChange functions - handles when user types in the dropdown
+         * @name onChange
+         * @memberof module: FilterComponent/Dropdown
+         * @function onChange
+         * @returns none
+        */
         this.onChange = (opt) => {
             this.props.setCourse(opt.value);
             this.setState({selectedCourse: opt});
         }
     }
 
+    /**
+     * Fetching courses from backend
+     * @name componentWillMount
+     * @memberof module: FilterComponent/Dropdown
+     * @function componentWillMount
+     * @returns list of courses from backend
+    */
     componentWillMount() {
         window.fetch('/api/course')
         .then(response=> response.json())
         .then(json=> this.setState({courses: json}));
     }
 
-
+    /**
+     *Render - provides UI for dropdown
+     * @name render
+     * @memberof module: FilterComponent/Dropdown
+     * @function render
+     * @returns UI
+    */
     render() {
         return(
             <div className="dropdown">

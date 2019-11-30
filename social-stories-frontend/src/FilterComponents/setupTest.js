fab5540b88ceffead4532dde07848afa6ec9c6ad
Rikki Augustine <rikkiaugustine@gmail.com>
2019-11-30 17:06:42 -0600

    HomeDisplay tests

--- social-stories-frontend/src/FilterComponents/setupTest.js
+++
@@ -1,7 +0,0 @@
-import Adapter from 'enzyme-adapter-react-16';
-import { configure } from 'enzyme';
-import fetch from './__mocks__/fetch';
- 
-global.fetch = fetch;
- 
-configure({adapter: new Adapter()});
